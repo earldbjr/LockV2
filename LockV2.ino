ccbe0f0eb9218bea80d93c309e6bdeb7fe8811f5
Gizmo <earldbjr@gmail.com>
2016-02-16 15:41:14 -0500

    BL timer added, many tweaks made, FPS library reverted.

--- LockV2.ino
+++ LockV2.ino
@@ -2,60 +2,60 @@
 
 #include <LiquidCrystal.h>
 #include <SPI.h>
-#include "MFRC522.h"
-#include "FPS_GT511C3nb.h"
+#include <MFRC522.h>
+#include <FPS_GT511C3.h> // My non-blocking version
 #include "SoftwareSerial.h"
-
+//RFID
 #define SS_PIN 10
 #define RST_PIN 9
+
 #define lcdBacklight 5
-#define FPSPower 4
 #define lockPin1 7
 #define lockPin2 8
-#define doorSensor 2
-
-long lcdBacklightTimer = 0;
-int isLocked = 0;
-String card1 = "2454512237";   //Given to Sun Bay office
-String card2 = "4201651025450129";
-String card3 = "166204210181"; //Zahrah's Keychain
-String card4 = "4201651025450129";
-String card5 = "4201651025450129";   //Gizmo's Wallet
-String card6 = "2454512237";
-
-MFRC522 rfid(SS_PIN, RST_PIN);
-LiquidCrystal lcd(A5,A4,A3,A2,A1,A0);
-FPS_GT511C3 fps(3,2);
-
-void setup()
-{
-  SPI.begin();
-  rfid.PCD_Init();
-
-  lcd.begin(16,2);
-
-  pinMode(FPSPower, OUTPUT);
-  digitalWrite(FPSPower, HIGH); //To be toggled later...
-  delay(100);
-  fps.Open();
-  delay(100);
-  fps.SetLED(true);
-}
+#define doorSensor 4
 /////Forward Declarations/////
 void checkBiometrics();
 void checkRFID();
 void unlockDoor();
 void lockDoor();
-void doorWatcher(); //Blocking!
+void doorWatcher(); //Blocking! For 5 sec.
 void lcdBacklightChecker();
 /////////////////////////////
 
+long lcdBacklightTimer = 0;
+int isLocked = 0;
+int lockTravelTime = 1200; //Time in mS for relay to be engaged.
+String card1 = "2454512237";   //Given to Sun Bay office
+String card2 = "101237102";  //Gizmo's Keychain
+String card3 = "166204210181"; //Zahrah's Keychain
+String card4 = "101237102";  //unused
+String card5 = "4171671025450129";   //Gizmo's Wallet
+String card6 = "2454512237"; //unused
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+LiquidCrystal lcd(A5, A4, A3, A2, A1, A0); //4-bit mode
+FPS_GT511C3 fps(2, 3);
+
+void setup()
+{
+  lcd.begin(16, 2);
+  pinMode(lcdBacklight, OUTPUT);
+  pinMode(lockPin1, OUTPUT);
+  pinMode(lockPin2, OUTPUT);
+  pinMode(doorSensor, INPUT);
+  digitalWrite(lcdBacklight, HIGH);
+  SPI.begin();
+  rfid.PCD_Init();
+  delay(100);
+  fps.Open();
+  delay(100);
+  fps.SetLED(true);
+  lockDoor();
+}
+
+
 void loop()
 {
-<<<<<<< HEAD
-  lcd.print("loop");
-=======
->>>>>>> acb04e6e0d5d9f9a61cc8e344ab7a56c0e644485
   checkBiometrics();
   checkRFID();
   lcdBacklightChecker();
@@ -67,29 +67,26 @@ void checkBiometrics()
   // Identify fingerprint
   if (fps.IsPressFinger())
   {
-    fps.CaptureFinger(false);
+    fps.CaptureFinger(true); //false indicates faster low-res mode
     int id = fps.Identify1_N();
-    if (id <200)
+    if (id < 200) //200 finger profiles
     {
       digitalWrite(lcdBacklight, HIGH);
       lcdBacklightTimer = millis();
       lcd.clear();
-      lcd.setCursor(0,0);
+      lcd.setCursor(0, 0);
       lcd.print("Verified ID:");
-      lcd.setCursor(0,1);
+      lcd.setCursor(0, 1);
       lcd.print(id);
       unlockDoor();
-<<<<<<< HEAD
       return;
-=======
->>>>>>> acb04e6e0d5d9f9a61cc8e344ab7a56c0e644485
     }
     else
     {
       digitalWrite(lcdBacklight, HIGH);
-      lcd.clear();
       lcdBacklightTimer = millis();
-      lcd.setCursor(0,0);
+      lcd.clear();
+      lcd.setCursor(0, 0);
       lcd.print("Invalid Finger");
       fps.SetLED(false);
       return;
@@ -102,31 +99,27 @@ void checkRFID()
   String idRead;
   // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
   MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) 
+  for (byte i = 0; i < 6; i++)
   {
     key.keyByte[i] = 0xFF;
   }
   // New card found && new card selected
   if ( rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
   {
-<<<<<<< HEAD
     lcd.clear();
-=======
-
->>>>>>> acb04e6e0d5d9f9a61cc8e344ab7a56c0e644485
-   lcd.setCursor(0,0);
-   lcd.print("Found!");
-   for (byte i = 0; i < rfid.uid.size; i++) 
+    lcd.setCursor(0, 0);
+    lcd.print("Found!");
+    for (byte i = 0; i < rfid.uid.size; i++)
     { // Dump UID for authentication
       idRead.concat(rfid.uid.uidByte[i]);
     }
     digitalWrite(lcdBacklight, HIGH);
     lcdBacklightTimer = millis();
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(idRead);
-    delay(5000);
+    delay(500);
 
-    if(
+    if (
       idRead == card1 ||
       idRead == card2 ||
       idRead == card3 ||
@@ -137,92 +130,88 @@ void checkRFID()
       digitalWrite(lcdBacklight, HIGH);
       lcdBacklightTimer = millis();
       lcd.clear();
-      lcd.setCursor(0,0);
+      lcd.setCursor(0, 0);
       lcd.print("Card Accepted!");
-      lcd.setCursor(0,1);
+      lcd.setCursor(0, 1);
       lcd.print(idRead);
-      if(isLocked == 1)
+      if (isLocked == 1)
       {
         unlockDoor();
-<<<<<<< HEAD
         return;
-=======
->>>>>>> acb04e6e0d5d9f9a61cc8e344ab7a56c0e644485
       }
-      } else 
-      {
-        digitalWrite(lcdBacklight, HIGH);
-        lcdBacklightTimer = millis();
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Invalid Card ID:");
-        lcd.setCursor(0,1);
-        lcd.print(idRead);
-<<<<<<< HEAD
-        return;
-=======
->>>>>>> acb04e6e0d5d9f9a61cc8e344ab7a56c0e644485
-      }
-
+    } else
+    {
+      digitalWrite(lcdBacklight, HIGH);
+      lcdBacklightTimer = millis();
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Invalid Card ID:");
+      lcd.setCursor(0, 1);
+      lcd.print(idRead);
+      return;
     }
   }
+}
 
-  void unlockDoor()
-  {
-    digitalWrite(lockPin1, LOW);
-    digitalWrite(lockPin2, HIGH);
-    isLocked = 0;
-    delay(1200);
-    digitalWrite(lockPin1, LOW);
-    digitalWrite(lockPin2, LOW);
-    doorWatcher();
-  }
 
 void doorWatcher()
 {
   long startTime = millis();
-  while(digitalRead(doorSensor) == 1)
+  while (digitalRead(doorSensor) == 1)
   {
-    if(millis() - startTime >= 10000) //Catch door being unlocked, but not opened for 10 seconds, and relock.
+    if (millis() - startTime >= 10000) //Catch door being unlocked, but not opened for 10 seconds, and relock.
     {
       lockDoor();
       return;
     }
   }
-  doorOpen:
-  while(digitalRead(doorSensor) == 0) //Door found to be open. Wait for door close.
+doorOpen:
+  while (digitalRead(doorSensor) == 0) //Door found to be open. Wait for door close.
   {
     //Add future alarm for door left unsecure? Flash LCDBacklight?
   }
   startTime = millis(); //New timer value for door-closed check.
-  while(millis() - startTime <= 2000) //Door re-closed. Scan while waiting, and relock.
+  while (millis() - startTime <= 2000) //Door re-closed. Scan while waiting, and relock.
   {
-    if(digitalRead(doorSensor) == 0)
+    if (digitalRead(doorSensor) == 0)
     {
-<<<<<<< HEAD
-      goto doorOpen;  
-=======
       goto doorOpen;
->>>>>>> acb04e6e0d5d9f9a61cc8e344ab7a56c0e644485
     }
   }
   //Door unlocked, opened, closed, and sat for 2 seconds:
   lockDoor();
 }
 
+void unlockDoor()
+{
+  if (digitalRead(doorSensor) == HIGH) //If door is closed...
+  {
+    digitalWrite(lockPin1, LOW);
+    digitalWrite(lockPin2, HIGH);
+    isLocked = 0;
+    delay(lockTravelTime);
+    digitalWrite(lockPin1, LOW);
+    digitalWrite(lockPin2, LOW);
+    doorWatcher(); //blocking for 10sec
+  }
+}
+
 void lockDoor()
 {
-  digitalWrite(lockPin1, HIGH);
-  digitalWrite(lockPin2, LOW);
-  isLocked = 1;
-  delay(1200); //Allow motor to engage.
-  digitalWrite(lockPin1, LOW);
-  digitalWrite(lockPin2, LOW);
+  if (digitalRead(doorSensor) == HIGH) //If door is closed...
+  {
+    digitalWrite(lockPin1, HIGH);
+    digitalWrite(lockPin2, LOW);
+    isLocked = 1;
+    delay(lockTravelTime); //Allow motor to engage.
+    digitalWrite(lockPin1, LOW);
+    digitalWrite(lockPin2, LOW);
+  }
 }
 
 void lcdBacklightChecker()
 {
-  if(millis() - lcdBacklightTimer >= 5000)
+  if (millis() - lcdBacklightTimer >= 5000)
   {
     lcd.clear();
     digitalWrite(lcdBacklight, LOW);
